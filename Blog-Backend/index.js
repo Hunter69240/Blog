1cf8faf8e0ce9a4ca54710e2a8f0975524949594
Hunter69240 <aadishds@gmail.com>
2026-02-26 13:09:49 +0530

    Basic running

---
+++ Blog-Backend/index.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const {Pool}=require("pg")
+const app=express()
+const port=3000
+
+// const pool = new Pool({
+//   user: process.env.DB_USER ,
+//   host: process.env.DB_HOST, 
+//   password: process.env.DB_PASSWORD ,
+//   port: process.env.DB_PORT,
+// });
+
+// pool.connect((err, client, done) => {
+//   if (err) throw err;
+//   console.log('Connected to PostgreSQL database!');
+ 
+// });
+app.get("/",(req,res)=>{
+    res.send("Server running");
+})
+
+app.listen(port,()=>{
+    console.log(`Running in  http://localhost:${port} ${process.env.DB_HOST}`)
+})
