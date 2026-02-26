22cf36e4e0651ce600b3654075c95f1821d585a7
Hunter69240 <aadishds@gmail.com>
2026-02-26 13:16:52 +0530

    Postgress connection

--- Blog-Backend/index.js
+++ Blog-Backend/index.js
@@ -1,24 +1,25 @@
 const express=require("express");
 const {Pool}=require("pg")
+require('dotenv').config();
 const app=express()
 const port=3000
 
-// const pool = new Pool({
-//   user: process.env.DB_USER ,
-//   host: process.env.DB_HOST, 
-//   password: process.env.DB_PASSWORD ,
-//   port: process.env.DB_PORT,
-// });
+const pool = new Pool({
+  user: process.env.DB_USER ,
+  host: process.env.DB_HOST, 
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
-// pool.connect((err, client, done) => {
-//   if (err) throw err;
-//   console.log('Connected to PostgreSQL database!');
+pool.connect((err, client, done) => {
+  if (err) throw err;
+  console.log('Connected to PostgreSQL database!');
  
-// });
+});
 app.get("/",(req,res)=>{
     res.send("Server running");
 })
 
 app.listen(port,()=>{
-    console.log(`Running in  http://localhost:${port} ${process.env.DB_HOST}`)
+    console.log(`Running in  http://localhost:${port} `)
 })
